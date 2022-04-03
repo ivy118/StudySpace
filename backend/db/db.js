fd9ab087fa3b7ed571848d62a6e96295fdee4fc5
JonathanCen <jcen3@binghamton.edu>
2022-04-03 17:04:02 -0400

    cleaned code in db

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,7 +1,4 @@
 const pg = require("pg");
-//postgres://cgpagjtj:xnWP_fT9RxbfqgUKKjeZRB2ggoGMeI0z@castor.db.elephantsql.com/cgpagjtj
-//cgpagjtj@castor.db.elephantsql.com
-//psql -h castor.db.elephantsql.com -p 5432 -d cgpagjtj -U cgpagjtj -W
 
 const pool = new pg.Pool({
   user: "cgpagjtj",
@@ -12,5 +9,8 @@ const pool = new pg.Pool({
 });
 
 module.exports = pool;
+<<<<<<< HEAD
 
 
+=======
+>>>>>>> cleaned code in db
